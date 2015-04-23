6b49e131ea015acf2723725062bc37d6573a67e9
developer <dev@zvitok.dev>
2015-04-24 02:59:18 +0300

    sources fixed from links to files

--- SHJSTerm.cs
+++ SHJSTerm.cs
@@ -1,1 +1,67 @@
-../WindowsFormsApplication1/SHJSTerm.cs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MagicSubmitter;
+
+namespace WindowsFormsApplication1
+{
+    class SHJSTerm
+    {
+
+        public FastProcessor processor;
+
+        public SHJSTerm()
+        {
+            processor = new FastProcessor() ;
+        }
+
+        public void run()
+        {
+            test();
+        }
+
+        public void test()
+        {
+            System.Diagnostics.Process jsProc = new System.Diagnostics.Process();
+            jsProc.StartInfo.FileName = System.IO.Directory.GetCurrentDirectory() + @"\xulrunner\js.exe";
+            if (!System.IO.File.Exists(jsProc.StartInfo.FileName))
+            {
+                jsProc.StartInfo.FileName = @"C:\Program Files\Alexandr Krulik\Magic Submitter\xulrunner\js.exe";
+            }
+            jsProc.StartInfo.Arguments = @"-e ""(function(){print('Line1');print('Reading Line:');var s = readline();print(\""hola!\"" + s)})()""";
+            jsProc.StartInfo.UseShellExecute = false;
+            jsProc.StartInfo.RedirectStandardOutput = true;
+            jsProc.StartInfo.RedirectStandardInput = true;
+            jsProc.StartInfo.CreateNoWindow = true;
+            jsProc.Start();
+            string line = "";
+            string[] lineParts;
+
+            while (!jsProc.StandardOutput.EndOfStream)
+            {
+                line = jsProc.StandardOutput.ReadLine();
+                lineParts = line.Split(new string[] { "<<<" }, StringSplitOptions.None);
+                if (lineParts.Length != 3)
+                {
+                    throw new Exception("Wrong request recieved: " + line);
+                }
+                try
+                {
+                    if (lineParts[1] == "OUTPUT")
+                    {
+                        MessageBox.Show(processor.GetJsonVal("data", lineParts[2]));
+                    }
+                    jsProc.StandardInput.WriteLine(">>>OK");
+                }
+                catch (Exception e)
+                {
+                    jsProc.StandardInput.WriteLine(">>>ERROR>>>" + e.Message);
+                }
+            }
+            jsProc.WaitForExit();       
+        }
+
+    }
+}
--- shjsterm.js
+++ shjsterm.js
@@ -1,1 +1,36 @@
-/var/www/htdocs/OKrulik/msphp/development/public_html/test/shjsterm.js
+var SHJSTerm = {
+    
+    COMMAND_OUTPUT : 'OUTPUT',
+    COMMAND_WRITEFILE : 'WRITEFILE',
+    COMMAND_HTTP_GET : 'HTTP_GET',
+    COMMAND_HTTP_POST : 'HTTP_POST',
+    COMMAND_SET_COOKIES : 'SET_COOKIES',
+    COMMAND_GET_COOKIES : 'GET_COOKIES',
+    DELIMITER_OUTPUT : '<<<',
+    DELIMITER_INPUT : '>>>',
+    RESULT_OK : 'OK',
+    RESULT_ERROR : 'ERROR',
+    
+    sendCommand: function(command, options)
+    {
+        var sendText = this.DELIMITER_OUTPUT + command + this.DELIMITER_OUTPUT + JSON.stringify(options);
+        print(sendText);
+        
+        var readText = '', readLine = readline();
+
+        while((readLine !== this.DELIMITER_INPUT + this.RESULT_OK) && (readLine.indexOf(this.DELIMITER_INPUT + this.RESULT_ERROR + this.DELIMITER_INPUT) !== 0))
+        {
+            readText += readLine;
+            readLine = readline();
+        }
+        if (readLine === this.DELIMITER_INPUT + this.RESULT_OK)
+        {
+            return {status: 1, data: readText};
+        }
+        else if (readLine.indexOf(this.DELIMITER_INPUT + this.RESULT_ERROR + this.DELIMITER_INPUT) === 0)
+        {
+            return {status: -1, message: readLine.substring((this.DELIMITER_INPUT + this.RESULT_ERROR + this.DELIMITER_INPUT).length), data: readText};
+        }        
+    }
+}
+
