d4acf989f2d1e092317372eca132caa66fb33ca7
developer <dev@zvitok.dev>
2015-04-24 15:26:42 +0300

    test updated

--- shjs_test.js
+++ shjs_test.js
@@ -1,4 +1,8 @@
-document.body.innerHTML = '<div class="g-recaptcha" data-sitekey="6LfFYAATAAAAALgkU2LlG2cMgBh1wASqutIc-5e3"></div>'
-                        + '<script src="https://www.google.com/recaptcha/api.js"></script>';
+document.body.innerHTML = '<div class="g-recaptcha" data-sitekey="6LfFYAATAAAAALgkU2LlG2cMgBh1wASqutIc-5e3"></div>';
+
+var jqs = document.createElement('script');
+jqs.type = 'text/javascript';
+jqs.src = 'https://www.google.com/recaptcha/api.js';
+document.getElementsByTagName('head')[0].appendChild(jqs);
 
 SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:document.innerHTML});
