3f4ad8ae61631f251fd0873f322d6d2b0130ac19
developer <dev@zvitok.dev>
2015-04-24 22:47:49 +0300

    more fix

--- shjs_test.js
+++ shjs_test.js
@@ -1,20 +1,20 @@
 try 
 {
-    document.body.innerHTML = '<div class="g-recaptcha" data-sitekey="6LfFYAATAAAAALgkU2LlG2cMgBh1wASqutIc-5e3" data-type="image"></div>';
+    //document.body.innerHTML = '<div class="g-recaptcha" data-sitekey="6LfFYAATAAAAALgkU2LlG2cMgBh1wASqutIc-5e3" data-type="image"></div>';
 
-    //document.body.innerHTML = '<div id="g-recaptcha-div"></div>';
+    document.body.innerHTML = '<div id="g-recaptcha-div"></div>';
 
     var jqs = document.createElement('script');
     jqs.type = 'text/javascript';
-    jqs.src = 'https://www.google.com/recaptcha/api.js';//?onload=onloadCallback&render=explicit';
+    jqs.src = 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit';
     document.getElementsByTagName('head')[0].appendChild(jqs);
 
-/*
+
     jqs = document.createElement('script');
     jqs.type = 'text/javascript';
     jqs.text = "grecaptcha.render('g-recaptcha-div', {sitekey : '6LfFYAATAAAAALgkU2LlG2cMgBh1wASqutIc-5e3'});";
     document.getElementsByTagName('head')[0].appendChild(jqs);
-    */
+    
 
     SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:document.innerHTML});
 
