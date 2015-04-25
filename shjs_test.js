712ae4e4ed185c2fb26978a0ae73ff3acec0cbb4
developer <dev@zvitok.dev>
2015-04-25 09:02:20 +0300

    env_js fixed bug: js script tags added to body via innerHTML were not executed

--- shjs_test.js
+++ shjs_test.js
@@ -34,40 +34,40 @@ try
 
     var jqs0 = document.createElement('script');
     jqs0.type = 'text/javascript';
-    jqs0.text = 'SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {"data":"HEAD script DOM 1"})';
+    jqs0.text = 'console.log("HEAD script DOM 1")';
     document.getElementsByTagName('head')[0].appendChild(jqs0);
 
     var jqs01 = document.createElement('script');
     jqs01.type = 'text/javascript';
-    jqs01.text = 'SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {"data":"HEAD script DOM 1_1"})';
+    jqs01.text = 'console.log("HEAD script DOM 1_1")';
     document.getElementsByTagName('head')[0].appendChild(jqs01);
 
 
-    document.head.innerHTML += '<script type="text/javascript">SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"HEAD script innerHTML 1"})</script>'
-                              +'<script type="text/javascript">SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"HEAD script innerHTML 2"})</script>';
+    document.head.innerHTML += '<script type="text/javascript">console.log("HEAD script innerHTML 1")</script>'
+                              +'<script type="text/javascript">console.log("HEAD script innerHTML 2")</script>';
 
     var jqs2 = document.createElement('script');
     jqs2.type = 'text/javascript';
-    jqs2.text = 'SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"HEAD script DOM 2"})';
+    jqs2.text = 'console.log("HEAD script DOM 2")';
     document.getElementsByTagName('head')[0].appendChild(jqs2);
 
     var jqs3 = document.createElement('script');
     jqs3.type = 'text/javascript';
-    jqs3.text = 'SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"BODY script DOM 1"})';
+    jqs3.text = 'console.log("BODY script DOM 1")';
     document.getElementsByTagName('body')[0].appendChild(jqs3);
 
     var jqs31 = document.createElement('script');
     jqs31.type = 'text/javascript';
-    jqs31.text = 'SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"BODY script DOM 1_1"})';
+    jqs31.text = 'console.log("BODY script DOM 1_1")';
     document.getElementsByTagName('body')[0].appendChild(jqs31);
 
-    document.body.innerHTML += '<script type="text/javascript">SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"BODY script innerHTML 1"})</script>'
+    document.body.innerHTML += '<script type="text/javascript">console.log("BODY script innerHTML 1")</script>'
                               +'<div id="maindiv1"></div>'
-                              +'<script type="text/javascript">SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"BODY script innerHTML 2"})</script>';
+                              +'<script type="text/javascript">console.log("BODY script innerHTML 2")</script>';
 
     var jqs4 = document.createElement('script');
     jqs4.type = 'text/javascript';
-    jqs4.text = 'SHJSTerm(SHJSTerm.COMMAND_OUTPUT, {data:"BODY script DOM 2"})';
+    jqs4.text = 'console.log("BODY script DOM 2")';
     document.getElementsByTagName('body')[0].appendChild(jqs4);
 
 
